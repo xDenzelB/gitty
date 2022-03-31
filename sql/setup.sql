2816f2b055cc0c9a99401d61a6ab906693a9484c
Denzel <dbartolaba123@gmail.com>
2022-03-31 12:38:01 -0700

    created sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS github_profile CASCADE;
+DROP TABLE IF EXISTS posts CASCADE;
+
+CREATE TABLE github_profile (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+    username TEXT NOT NULL,
+    email TEXT,
+    avatar TEXT
+); 
+
+CREATE TABLE posts (
+    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    post TEXT NOT NULL,
+    username TEXT NOT NULL,
+    avatar TEXT
+)

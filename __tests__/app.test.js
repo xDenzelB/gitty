2cbfa5dac5510fc7a460eee9fec4522144f6e6c9
Denzel <dbartolaba123@gmail.com>
2022-03-31 16:28:13 -0700

    added in test for redirect to dashboard

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -21,6 +21,20 @@ describe('gitty routes', () => {
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
   });
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const req = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
 
+    expect(req.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
 
 });
